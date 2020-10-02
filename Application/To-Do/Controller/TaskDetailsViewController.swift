720885d616ccfca2262c6a1ad62c1d9623c56417
Ankith Madadi <ankithm28@gmail.com>
2020-10-01 23:14:56 -0700

    Added submit on return

--- Application/To-Do/Controller/TaskDetailsViewController.swift
+++ Application/To-Do/Controller/TaskDetailsViewController.swift
@@ -13,7 +13,7 @@ protocol TaskDelegate: class {
     func didTapUpdate(task : Task)
 }
 
-class TaskDetailsViewController: UIViewController {
+class TaskDetailsViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     
     // OUTLETS
     @IBOutlet weak var taskTitleTextField: UITextField!
@@ -37,6 +37,8 @@ class TaskDetailsViewController: UIViewController {
         subTasksTextView.addBorder()
         loadTaskForUpdate()
         saveButton.title = (task == nil) ? "Add" : "Update"
+        taskTitleTextField.delegate = self
+        taskTitleTextField.tag = 1
         // Tap outside to close the keybord
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
@@ -81,4 +83,15 @@ class TaskDetailsViewController: UIViewController {
         endDateTextField.text = endDate
     }
     
+}
+extension TaskDetailsViewController {
+func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//Check if there is any other text-field in the view whose tag is +1 greater than the current text-field on which the return key was pressed. If yes → then move the cursor to that next text-field. If No → Dismiss the keyboard
+if let nextField = self.view.viewWithTag(textField.tag + 1) as? UITextField {
+nextField.becomeFirstResponder()
+} else {
+textField.resignFirstResponder()
+}
+return false
+}
 }

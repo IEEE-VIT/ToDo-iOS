7abded3099175535a9978b3eb957ad9b3f7c6c9d
Tejeshwar Amirthy <tejuamirthi@gmail.com>
2020-10-02 12:34:54 +0530

    Added Search bar for searching tasks. (#31)
    * Added Search bar for searching tasks when the list grows.
    
    Signed-off-by: tejuamirthi <tejuamirthi@gmail.com>
    
    * Used filter replacing foreach loop
    Fixed requested changes.
    
    Signed-off-by: tejuamirthi <tejuamirthi@gmail.com>

--- Application/To-Do/Controller/TodoViewController.swift
+++ Application/To-Do/Controller/TodoViewController.swift
@@ -13,6 +13,12 @@ class TodoViewController: UITableViewController {
     /// `Tableview` to display list of tasks
     @IBOutlet weak var todoTableView: UITableView!
     
+    /// `SearchController` to include search bar
+    var searchController: UISearchController!
+    
+    /// `ResultsController` to display results of specific search
+    var resultsTableController: ResultsTableController!
+       
     /// `DataSource` for todoTableview
     var todoList : [Task] = []
     var lastIndexTapped : Int = 0
@@ -25,10 +31,14 @@ class TodoViewController: UITableViewController {
     
     //MARK: View lifecycle methods
     override func viewDidLoad() {
+        setupSearchController()
         super.viewDidLoad()
-        // TODO: initial setup if any
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.navigationItem.searchController = searchController
+    }
     
     //MARK: IBActions
     /// perform segue to `TaskDetailsViewController` if `+` tapped
@@ -56,6 +66,17 @@ class TodoViewController: UITableViewController {
         }
     }
     
+    /// search controller setup
+    fileprivate func setupSearchController() {
+        resultsTableController =
+            self.storyboard?.instantiateViewController(withIdentifier: "ResultsTableController") as? ResultsTableController
+        resultsTableController.tableView.delegate = self
+        searchController = UISearchController(searchResultsController: resultsTableController)
+        searchController.delegate = self
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+        searchController.searchBar.delegate = self
+    }
     
     //MARK:  ------ Tableview Datasource methods ------
     // Reference: https://developer.apple.com/documentation/uikit/uitableviewdatasource
@@ -116,5 +137,29 @@ extension TodoViewController : TaskDelegate{
     func didTapUpdate(task: Task) {
         todoList[lastIndexTapped] = task /// edit task
         tableView.reloadData() /// Reload tableview with new data
+    }
+}
+
+// MARK: - Search Bar Delegate
+
+extension TodoViewController: UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        /// perform search only when there is some text
+        if let text: String = searchController.searchBar.text?.lowercased(), text.count > 0, let resultsController = searchController.searchResultsController as? ResultsTableController {
+            resultsController.todoList = todoList.filter({ (task) -> Bool in
+                if task.title.lowercased().contains(text) || task.subTasks.lowercased().contains(text) {
+                    return true
+                }
+                return false
+            })
+            resultsController.tableView.reloadData()
+        } else {
+            /// default case when text not available or text length is zero
+            tableView.reloadData()
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        tableView.reloadData()
     }
 }

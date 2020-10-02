f2c835d7bcf2dab91c2c0efc22bb1dcb2fb9e2de
Adriana <melina.gzz@hotmail.com>
2020-10-02 01:57:30 -0700

    Fix Storyboard issue with SF symbol

--- Application/To-Do/View/TaskCell.swift
+++ Application/To-Do/View/TaskCell.swift
@@ -17,12 +17,6 @@ class TaskCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.starImage.image = UIImage(systemName: "star.fill")
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }

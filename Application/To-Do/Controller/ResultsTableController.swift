cdad949ade7682ad339ebb530efe19e47a98dd55
NiklasOemler <55978783+NiklasOemler@users.noreply.github.com>
2020-10-10 14:31:39 +0200

    Add empty fetch result state (#57) (#64)
    Co-authored-by: Niklas Oemler <niklas.oemler@gal-digital.de>

--- Application/To-Do/Controller/ResultsTableController.swift
+++ Application/To-Do/Controller/ResultsTableController.swift
@@ -13,7 +13,13 @@ class ResultsTableController: UITableViewController {
     
     var todoList = [Task]()
     
+    
+    //MARK: UITableView DataSource
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        self.todoList.count == 0 ? self.showEmptyState() : self.hideEmptyState()
+        
         return todoList.count
     }
     
@@ -23,5 +29,21 @@ class ResultsTableController: UITableViewController {
         cell.textLabel?.text = task.title
         cell.detailTextLabel?.text = task.dueDate
         return cell
+    }
+    
+    //MARK : Fetch Result State Handling
+    
+    private func showEmptyState() {
+        
+        let emptyResultLabel = UILabel()
+        emptyResultLabel.translatesAutoresizingMaskIntoConstraints = false
+        emptyResultLabel.text = "No Search results!"
+        emptyResultLabel.textColor = .black
+        emptyResultLabel.textAlignment = .center
+        self.tableView.backgroundView = emptyResultLabel
+    }
+    
+    private func hideEmptyState() {
+        self.tableView.backgroundView = nil
     }
 }

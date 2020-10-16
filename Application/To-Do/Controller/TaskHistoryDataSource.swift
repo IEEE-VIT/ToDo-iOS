43e91b04a89d78ce8bf7e7e044a655185e33f658
Alexis <al.orellano23@gmail.com>
2020-10-16 13:27:57 -0700

    task history datasource seperated

---
+++ Application/To-Do/Controller/TaskHistoryDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  TaskHistoryDataSource.swift
+//  To-Do
+//
+//  Created by Alexis Orellano on 10/16/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
---
+++ Application/To-Do/Controller/TodoDataSouce.swift
@@ -0,0 +1,9 @@
+//
+//  TodoDataSouce.swift
+//  To-Do
+//
+//  Created by Alexis Orellano on 10/16/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
